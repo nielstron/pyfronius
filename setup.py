a2731f3752794aa3b8e7273d53212ae2478b040c
nielstron <n.muendler@web.de>
2018-01-04 00:27:48 +0100

    Changed the topic
    The topic used befor (Interfacing) was not allowed by pypi so I changed it to "Object Brokering" which seems to be pretty much what we do here. http://wiki.c2.com/?ObjectBroker

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # To use a consistent encoding
 from codecs import open
@@ -14,25 +14,23 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='PyFronius',
-      version='0.2',
+      version='0.1',
       description='Automated JSON API based communication with Fronius Symo',
-      author='Niels Mündler, Gerrit Beine',
-      author_email='n.muendler@web.de, mail@gerritbeine.de',
+      author='Niels Mündler',
+      author_email='n.muendler@web.de',
       url='https://github.com/nielstron/pyfronius/',
       py_modules=['pyfronius'],
-      packages = find_packages(),
-      requires=[
+      requires= [
           'json',
-          'aiohttp',
-          'logging',
-        ],
+          'requests'
+          ],
       long_description=long_description,
       license='MIT',
       classifiers=[
         'Development Status :: 3 - Alpha',
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Interfacing',
+        'Topic :: Software Development :: Object Brokering',
     
         # Pick your license as you wish (should match "license" above)
          'License :: OSI Approved :: MIT License',
@@ -41,6 +39,6 @@ setup(name='PyFronius',
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.6'
         ],
-      keywords='python fronius json api solar photovoltaics pv',
+      keywords='python fronius json api solar photovoltaic pv',
       python_requires='>=3',
      )
