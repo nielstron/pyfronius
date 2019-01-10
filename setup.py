b093d51beff2b78ef7890c1c4fb1a34a386a1259
Niels Mündler <n.muendler@tum.de>
2019-01-11 00:01:03 +0100

    Remove python 3.4 test as aiohttp is incompatible with 3.4

--- setup.py
+++ setup.py
@@ -33,11 +33,13 @@ setup(name='PyFronius',
         'Topic :: Software Development :: Object Brokering',
     
         # Pick your license as you wish (should match "license" above)
-         'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT License',
     
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         ],
       keywords='python fronius json api solar photovoltaics pv',
       python_requires='>=3',
