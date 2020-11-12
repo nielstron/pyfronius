937e742df67f195b56db77e895dda8e8b553bf4c
Niels Mündler <n.muendler@tum.de>
2020-11-12 02:36:09 +0100

    Remove python 3.5
    python3.5 is not officially supported by aiohttp and therefore transitively not by this project

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Object Brokering",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
